06f5e9ac3de07b40d0fbcf7736f5106f1ec6e622
JSew11 <jseward@zagmail.gonzaga.edu>
2022-04-21 17:39:32 +0000

    started exploratory EDA

---
+++ plot_utils.py
@@ -0,0 +1,150 @@
+import matplotlib.pyplot as plt
+from sympy import rotations
+
+import utils
+
+def plot_bar_chart(x_values: list, y_values: list, title: str=None, x_axis_name: str = None,
+    y_axis_name: str = None, x_tick_labels:list = None, y_tick_labels: list = None):
+    """Plots a bar graph using matplotlib.
+
+    Args:
+        x_values (list): list of values to plot along the x-axis
+        y_values (list): list of values to plot along the y-axis
+        title (str): OPTIONAL title to display on the graph 
+        x_axis_name (str): OPTIONAL name for the x-axis
+        y_axis_name (str): OPTIONAL name for the y-axis
+        x_tick_labels (list of str): OPTIONAL parallel list to x-axis values for the names at each 
+        value
+        y_tick_labels (list of str): OPTIONAL parallel list to y-axis values for the names at each 
+        value
+    """
+    plt.figure()
+    plt.bar(x_values, y_values)
+    if title != None:
+        plt.title(title)
+    if x_axis_name != None:
+        plt.xlabel(x_axis_name)
+    if y_axis_name != None:
+        plt.ylabel(y_axis_name)
+    if x_tick_labels != None:
+        plt.xticks(x_values, labels=x_tick_labels, rotation=-75)
+    if y_tick_labels != None:
+        plt.yticks(y_values, labels=y_tick_labels, rotation=-75)
+    plt.show()
+
+def plot_pie_chart(values: list, labels: list, title: str = None):
+    """Plots pie chart using matplotlib.
+
+    Args:
+        values (list): list of values to display in the pie chart
+        labels (list): list of labels to display along the outside of the pie chart
+        title (str): OPTIONAL title to display on the pie chart
+    """
+    plt.figure()
+    plt.pie(values, labels=labels, autopct='%1.1f%%')
+    if title != None:
+        plt.title(title)
+    plt.legend(labels=labels, bbox_to_anchor=(0.85,1.025), loc="upper left")
+    plt.show()
+
+def plot_equal_width_frequency_diagram(cutoffs: list, frequencies: list, title: str = None,
+    x_axis_name: str = None, y_axis_name:str = None, x_tick_labels: list = None, 
+    y_tick_labels: list = None):
+    """Plots a frequency diagram using equal width cutoffs and matplotlib.
+
+    Args:
+        cutoffs (list): list of cutoff values to define the "bins"
+        frequencies (list): list of values that represent the number of things that fall in each 
+        "bin"
+        title (str): OPTIONAL title to display on the graph 
+        x_axis_name (str): OPTIONAL name for the x-axis
+        y_axis_name (str): OPTIONAL name for the y-axis
+        x_tick_labels (list of str): OPTIONAL parallel list to x-axis values for the names at each 
+        value
+        y_tick_labels (list of str): OPTIONAL parallel list to y-axis values for the names at each 
+        value
+    """
+    plt.figure()
+    plt.bar(cutoffs[:-1], frequencies, width=(cutoffs[1]-cutoffs[0]), edgecolor='black')
+    if title != None:
+        plt.title(title)
+    if x_axis_name != None:
+        plt.xlabel(x_axis_name)
+    if y_axis_name != None:
+        plt.ylabel(y_axis_name)
+    if x_tick_labels != None:
+        plt.xticks(cutoffs[:-1], labels=x_tick_labels, rotation=-45)
+    if y_tick_labels != None:
+        plt.xticks(cutoffs[:-1], labels=y_tick_labels)
+    plt.show()
+
+def plot_histogram(data: list, title: str = None, x_axis_name: str = None, 
+    y_axis_name: str = None):
+    """Plots a histogram using matplotlib.
+
+    Args:
+        data (list): data to be plotted on the histogram
+        title (str): OPTIONAL title to display on the graph 
+        x_axis_name (str): OPTIONAL name for the x-axis
+        y_axis_name (str): OPTIONAL name for the y-axis
+    """
+    plt.figure()
+    plt.hist(data, bins=10)
+    if title != None:
+        plt.title(title)
+    if x_axis_name != None:
+        plt.xlabel(x_axis_name)
+    if y_axis_name != None:
+        plt.ylabel(y_axis_name)
+    plt.show()
+
+def plot_scatter_chart(x_values: list, y_values: list, title: str = None, 
+    x_axis_name: str = None, y_axis_name: str = None):
+    """Plots a scatter chart using matplotlib. Calculates and shows a fit line created using 
+    linear regression..
+
+    Args:
+        x_values (list):
+        y_values (list):
+        title (str): OPTIONAL title to display on the graph 
+        x_axis_name (str): OPTIONAL name for the x-axis
+        y_axis_name (str): OPTIONAL name for the y-axis
+    """
+    plt.figure()
+    plt.scatter(x_values, y_values, marker='o', s=100)
+    if title != None:
+        plt.title(title)
+    if x_axis_name != None:
+        plt.xlabel(x_axis_name)
+    if y_axis_name != None:
+        plt.ylabel(y_axis_name)
+    slope, intercept = utils.compute_slope_intercept(x=x_values, y=y_values)
+    plt.plot([min(x_values), max(x_values)], 
+        [slope * min(x_values) + intercept, slope * max(x_values) + intercept], c="r", lw=3)
+    plt.tight_layout()
+    plt.show()
+
+def plot_box_chart(distributions: list, labels:list, title: str = None, x_axis_name: str = None,
+    y_axis_name: str = None):
+    """Plots a box chart using matplotlib.
+
+    Args:
+        distributions (list):
+        labels (list):
+        title (str): OPTIONAL title to display on the graph
+        x_axis_name (str): OPTIONAL name for the x-axis
+        y_axis_name (str): OPTIONAL name for the y-axis
+    """
+    plt.figure()
+    plt.boxplot(distributions)
+    if title != None:
+        plt.title(title)
+    if x_axis_name != None:
+        plt.xlabel(x_axis_name)
+    if y_axis_name != None:
+        plt.ylabel(y_axis_name)
+    plt.xticks(list(range(1, len(distributions) + 1)), labels, rotation=-75)
+    plt.annotate("$\mu=100$", xy=(1.5, 105), xycoords="data", horizontalalignment="center")
+    plt.annotate("$\mu=100$", xy=(0.5, 0.5), xycoords="axes fraction", 
+                 horizontalalignment="center", color="blue")
+    plt.show()
---
+++ utils.py
@@ -0,0 +1,179 @@
+import numpy as np
+
+from mypytable import MyPyTable
+
+def get_column_list(table: list, header: list, col_name: str):
+    """Gets the values in the desired column from a given 2D list of values.
+
+    Args:
+        table (list): 2D list of values
+        header (list): header for the table
+        col_name (str): nameof the desired column
+
+    Returns:
+        list: values in the desired column
+    """
+    column = []
+    column_index = header.index(col_name)
+    for row in table:
+        column.append(row[column_index])
+    return column
+
+def get_frequencies(table:MyPyTable, col_name: str):
+    """Gets the frequencies for each value of a numeric attribute. Gets
+    sorted list of all values in the given column of the given table.
+    Goes through sorted list and for each occurence, adds one to the 
+    corresponding index of the occurrences list.
+
+    Args:
+        col_name (str): name of the column we are searching
+
+    Return:
+        dict: keys are the values in the column and values are the number 
+        of times that they occur
+    """
+    col_frequencies = dict()
+    col_data = table.get_column(col_identifier=col_name)
+    for value in col_data:
+        if value in col_frequencies.keys():
+            col_frequencies[value] += 1
+        else:
+            col_frequencies[value] = 1
+    return list(col_frequencies.keys()), list(col_frequencies.values())
+
+def group_by(table: list, header: list, col_name: str):
+    """Groups rows in the given table together using the given column name.
+    Gets the index and column data of the desired index. Gets the "names" 
+    of the groups using the values in the column (converts them to an ordered
+    list of unique values). Goes through rows in the given table, grouping them 
+    by their value in the desired column.
+    
+    Args:
+        table (MyPyTable): table that we are searching
+        col_name (str): name of the column we are searching
+        
+    Returns:
+        list of list of list: ordered list of unique values in the desired column
+        list of list: list of subtables group by the value in the desired
+        column
+    """
+    group_by_col_index = header.index(col_name)
+    group_by_col = get_column_list(table=table, header=header, col_name=col_name)
+    group_by_split_col = []
+    for value in group_by_col:
+        split_value = value.split(",")
+        for item in split_value:
+            group_by_split_col.append(item)
+    group_names = sorted(list(set(group_by_split_col)))
+    group_subtables = [[] for _ in group_names]
+    for row in table:
+        group_by_val = row[group_by_col_index].split(",")
+        for val in group_by_val:
+            group_by_val_subtable_index = group_names.index(val)
+            group_subtables[group_by_val_subtable_index].append(row.copy())
+    return group_names, group_subtables
+
+def compute_equal_width_cutoffs(values: list, number_of_bins: int):
+    """Compute the cutoffs of "equal width" binning discretization. Gets the 
+    width of the bins using the range of the given values and the given number 
+    of bins. 
+
+    Args:
+        values (list): values to create bins from
+        number_of_bins (int): the number of desired equal-width bins
+
+    Returns:
+        list of float: cutoff values for equal width bins
+    """
+    values_range = max(values) - min(values)
+    bin_width = values_range / number_of_bins
+    # use np.arrage b/c getting a range of floats
+    cutoffs = list(np.arange(min(values), max(values), bin_width))
+    cutoffs.append(max(values))
+    cutoffs = [round(cutoff, 2) for cutoff in cutoffs]
+    return cutoffs
+
+def compute_bin_frequencies(values: list, cutoffs: list):
+    """Compute the frequencies for each value in their respective bin as a part of 
+    "equal width" binning discretization. Iterates through values and increases the 
+    frequency of the corresponding cutoff from the given cutoffs table as necessary.
+
+    Args:
+        values (list): values used to count frequencies
+        cutoffs (list of float): cutoff values of bins
+
+    Returns:
+        list of int: number of occurences that are in each "bin"
+    """
+    frequencies = [0 for _ in range(len(cutoffs)-1)] # because N + 1 cutoffs
+    for value in values:
+        if value == max(values): # fully closed last bin
+            frequencies[-1] += 1
+        else:
+            for index in range(len(cutoffs)-1): # remaining half-open bins
+                if cutoffs[index] <= value and value < cutoffs[index + 1]:
+                    frequencies[index] += 1
+    return frequencies
+
+def compute_slope_intercept(x: list, y: list):
+    """Computes the slope and y-intercept for a group of given values. Mostly used 
+    to create a trendline.
+
+    Args:
+        x (list of float): represents x values of points
+        y (list of float): represents y values of points
+
+    Returns:
+        float: slope of trendline of points
+        float: y-intercept of trendline of points
+    """
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    numerator = sum([(x[index] - mean_x) * (y[index] - mean_y) for index in range(len(x))])
+    denominator = sum([(x[index] - mean_x) ** 2 for index in range(len(x))])
+    m = numerator / denominator
+    b = mean_y - m * mean_x
+    return m, b
+
+def compute_covariance(x_values: list, y_values: list):
+    """Computes the covariance for the parallel lists given.
+
+    Args:
+        x_values (list): list of x values
+        y_values (list): list of y values
+
+    Returns:
+        float: covariance between the given lists of values
+    """
+    x_mean = np.mean(x_values)
+    y_mean = np.mean(y_values)
+    numerator = 0
+    for index in range(len(x_values)):
+        numerator += (x_values[index] - x_mean) * (y_values[index] - y_mean)
+    denominator = len(x_values)
+    return numerator / denominator
+
+def compute_correlation_coefficient(x_values: list, y_values: list):
+    """Computes the correlation coefficient for the parallel lists given.
+
+    Args:
+        x_values (list): list of x values
+        y_values (list): list of y values
+
+    Returns:
+        float: correlation coefficient between the given lists of values
+    """
+    x_mean = np.mean(x_values)
+    y_mean = np.mean(y_values)
+    numerator = 0
+    for index in range(len(x_values)):
+        numerator += (x_values[index] - x_mean) * (y_values[index] - y_mean)
+    denominator_x = 0
+    for index in range(len(x_values)):
+        denominator_x += (x_values[index] - x_mean) ** 2
+    denominator_y= 0
+    for index in range(len(y_values)):
+        denominator_y += (y_values[index] - y_mean) ** 2
+    denominator = np.sqrt(denominator_x * denominator_y)
+    return numerator / denominator
+
